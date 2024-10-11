7a5f8fcd20c5a5763d4ae0d2fcfe6dca6286c137
watstyle <tylerscottwatson@gmail.com>
2024-10-11 11:58:29 -0700

    Initial Commit

---
+++ Mesh/MeshApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeshApp.swift
+//  Mesh
+//
+//  Created by Tyler Watson on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MeshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
